a1c9fd38bf9238c4aa611f4bd4819bc9636a3474
ryan-hung <hongcan0824@gmail.com>
2016-11-21 23:20:20 +0800

    complete home components

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -66,10 +66,163 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xs-offset-1 col-xs-10 col-sm-offset-4 col-sm-4 col-md-offset-5 col-md-2">
-                    <a href="#" class="link-more">查看更多</a>
+            <div class="row text-center">
+                <a href="#" class="link-more">查看更多</a>
+            </div>
+            <div class="row text-center">
+                <img src="img/process.png" alt="" class="img-responsive img-process">
+            </div>
+        </div>
+    </section>
+    <section class="demos">
+        <div class="container">
+            <div class="row text-center">
+                <h3>案例精选</h3>
+            </div>
+            <div class="row m-t-xl demo-wrapper">
+                <div class="col-md-6">
+                    <div class="demo demo-main">
+                        <img src="img/demos-1.png" alt="" class="img-responsive">
+                        <div class="demo-footer">
+                            <h4>
+                                携益博睿、阿里金融云发布大数据风控平台
+                            </h4>
+                            <p class="date">
+                                2015-09-14
+                            </p>
+                            <p class="content">
+                                2015年4月16日，旨在打造中国小微金融机构“金融IT、大数据征信和互联网服务”铁人三项能力的“神州融大数据风控平台“在京发布，该平台由神州融与全球最大征信局Experian（益博睿）、阿里金融云联合各大征信机构、电商平台等伙伴合作打造，小微金融机构零门槛即可拥有价值千万、世界顶级信贷工厂管理系统与量化风控决策能力，一站式轻松接入3000+维度鲜活数据源和自动化决策评分卡，并免费对接大型电商平台、获得垂直信贷场景下的创新金融产品。
+                            </p>
+                        </div>
+                    </div>
                 </div>
+                <div class="col-md-6">
+                    <div class="row demo-other">
+                        <div class="col-md-6">
+                            <div class="demo">
+                                <img src="img/demos-2.png" alt="" class="img-responsive">
+                                <div class="demo-footer">
+                                    <h4 class="text-ellipsis">
+                                        携益博睿、阿里金融云发布大数据风控平台
+                                    </h4>
+                                    <p class="date">
+                                        2015-09-14
+                                    </p>
+                                    <p class="content">
+                                        零门槛拥有价值千万、世界顶级信贷工厂管理系统与量化风控决策能力，一站式轻松接入3000+维度鲜活数据源。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="demo">
+                                <img src="img/demos-2.png" alt="" class="img-responsive">
+                                <div class="demo-footer">
+                                    <h4 class="text-ellipsis">
+                                        携益博睿、阿里金融云发布大数据风控平台
+                                    </h4>
+                                    <p class="date">
+                                        2015-09-14
+                                    </p>
+                                    <p class="content">
+                                        零门槛拥有价值千万、世界顶级信贷工厂管理系统与量化风控决策能力，一站式轻松接入3000+维度鲜活数据源。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="demo">
+                                <img src="img/demos-2.png" alt="" class="img-responsive">
+                                <div class="demo-footer">
+                                    <h4 class="text-ellipsis">
+                                        携益博睿、阿里金融云发布大数据风控平台
+                                    </h4>
+                                    <p class="date">
+                                        2015-09-14
+                                    </p>
+                                    <p class="content">
+                                        零门槛拥有价值千万、世界顶级信贷工厂管理系统与量化风控决策能力，一站式轻松接入3000+维度鲜活数据源。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="demo">
+                                <img src="img/demos-2.png" alt="" class="img-responsive">
+                                <div class="demo-footer">
+                                    <h4 class="text-ellipsis">
+                                        携益博睿、阿里金融云发布大数据风控平台
+                                    </h4>
+                                    <p class="date">
+                                        2015-09-14
+                                    </p>
+                                    <p class="content">
+                                        零门槛拥有价值千万、世界顶级信贷工厂管理系统与量化风控决策能力，一站式轻松接入3000+维度鲜活数据源。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="news">
+        <div class="container">
+            <div class="row text-center">
+                <h3>江山新闻</h3>
+                <p class="news-subtitle">
+                    副标题占坑
+                </p>
+            </div>
+            <div class="row">
+                <div class="news-item">
+                    <img src="img/news.jpg" alt="" class="img-responsive pull-left">
+                    <div class="pull-left">
+                        <p class="date">2016-9-29</p>
+                        <h4>这里是一个新闻标题</h4>
+                        <p class="content">
+                            深圳、东莞、广州、重庆、北京月收入：拥有稳定收入，及良好的信用记录...
+                        </p>
+                        <a href="#" class="link-detail">详情 >></a>
+                    </div>
+                </div>
+                <div class="news-item">
+                    <img src="img/news.jpg" alt="" class="img-responsive pull-left">
+                    <div class="pull-left">
+                        <p class="date">2016-9-29</p>
+                        <h4>这里是一个新闻标题</h4>
+                        <p class="content">
+                            深圳、东莞、广州、重庆、北京月收入：拥有稳定收入，及良好的信用记录...
+                        </p>
+                        <a href="#" class="link-detail">详情 >></a>
+                    </div>
+                </div>
+                <div class="news-item">
+                    <img src="img/news.jpg" alt="" class="img-responsive pull-left">
+                    <div class="pull-left">
+                        <p class="date">2016-9-29</p>
+                        <h4>这里是一个新闻标题</h4>
+                        <p class="content">
+                            深圳、东莞、广州、重庆、北京月收入：拥有稳定收入，及良好的信用记录...
+                        </p>
+                        <a href="#" class="link-detail">详情 >></a>
+                    </div>
+                </div>
+                <div class="news-item">
+                    <img src="img/news.jpg" alt="" class="img-responsive pull-left">
+                    <div class="pull-left">
+                        <p class="date">2016-9-29</p>
+                        <h4>这里是一个新闻标题</h4>
+                        <p class="content">
+                            深圳、东莞、广州、重庆、北京月收入：拥有稳定收入，及良好的信用记录...
+                        </p>
+                        <a href="#" class="link-detail">详情 >></a>
+                    </div>
+                </div>
+            </div>
+            <div class="row text-center">
+                <a href="#" class="link-more">查看更多</a>
             </div>
         </div>
     </section>
--- resources/assets/js/components/layout/AppFooter.vue
+++ resources/assets/js/components/layout/AppFooter.vue
@@ -1,22 +1,5 @@
 <template>
-    <footer>
-        <h1>{{footer}}</h1>
+    <footer class="ks-footer">
+
     </footer>
 </template>
-<script>
-export default {
-    created() {
-        this.setFooter()
-    },
-    data() {
-        return {
-            footer: ''
-        }
-    },
-    methods: {
-        setFooter() {
-            this.$set('footer', 'this is footer!');
-        }
-    }
-}
-</script>
